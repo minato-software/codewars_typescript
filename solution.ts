4eda98a9c20337e1eb308b8b2f84d57338d05109
Zach Groves <fluffygorawr14@gmail.com>
2026-02-05 16:42:58 -0600

    Grasshopper - Summation

--- solution.ts
+++ solution.ts
@@ -1,7 +1,7 @@
 export const summation = (num:number)=> {
     let sum = 0;
-    for(let i = 1; i <= num; i++) {
-        sum += 1;
+    for (let i = 1; i <= num; i++) {
+        sum += i;
     }
     return sum;
 }
