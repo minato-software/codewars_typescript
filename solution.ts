9851e5d7ab2d8acf1979b1b89683c7d3493da9b3
Zach Groves <fluffygorawr14@gmail.com>
2026-02-10 13:19:23 -0600

    I love you, a little , a lot, passionately ... not at all

--- solution.ts
+++ solution.ts
@@ -1,9 +1,4 @@
-export function countSheeps(arrayOfSheep: (boolean | undefined | null)[]): number {
-    let count = arrayOfSheep.reduce((accumulator, sheep) => {
-        if(sheep != undefined && sheep != null && sheep) {
-            return accumulator + 1;
-        }
-        return accumulator;
-    }, 0); // 0 refers to the initial value of the accumulator, which is the count of sheep.
-    return count;
+export function howMuchILoveYou(petals: number): string{
+    const phrases = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
+    return phrases[(petals - 1) % phrases.length];
 }
