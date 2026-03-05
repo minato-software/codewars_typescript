81a2a6ed7bc10ab5a91c2773905a5e467817839e
Zach Groves <fluffygorawr14@gmail.com>
2026-03-05 14:14:47 -0600

    "Two Oldest Ages"

--- solution.ts
+++ solution.ts
@@ -1,9 +1,23 @@
-export const digitalRoot = (n:number):number => {
-    const digits = String(n).split('').map(Number);
-    let sum = digits.reduce((acc, num) => acc + num, 0);
-    while (sum >= 10) {
-        const loops = sum.toString().split('').map(Number);
-         sum = loops.reduce((acc, num) => acc + num, 0);
+// return the two oldest/oldest ages within the array of ages passed in.
+// it should return the two ages as a sorted array, youngest age first
+export function twoOldestAges(ages: number[]): number[] {
+    let max = Math.max.apply(Math, ages);
+    let nextBiggestAge = nextBiggest(ages);
+    let oldestAges: number[] = [nextBiggestAge, max];
+    return oldestAges;
+}
+function nextBiggest(arr :number[]) {
+    let max = -Infinity, result = -Infinity;
+
+    for (const value of arr) {
+        const nr = Number(value)
+
+        if (nr >= max) {
+            [result, max] = [max, nr] // save previous max
+        } else if (nr < max && nr > result) {
+            result = nr; // new second biggest
+        }
     }
-    return sum;
-};
+
+    return result;
+}
--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
-import {assert} from "chai";
-import {digitalRoot} from './solution';
+import { twoOldestAges } from './solution';
+import chai, { assert } from "chai";
+chai.config.truncateThreshold = 0;
 
-describe("solution", () => {
-    it('should work for fixed tests', () => {
-        assert.equal(digitalRoot(16), 7);
-        assert.equal(digitalRoot(456), 6 );
-        assert.equal(digitalRoot(426890740096), 1 );
+describe("Two Oldest Age", () => {
+    it("twoOldestAges([1,5,87,45,8,8]) should return [45,87]", () => {
+        assert.deepEqual(twoOldestAges([1, 5, 87, 45, 8, 8]), [45, 87]);
+        assert.deepEqual(twoOldestAges([18,48,7,40,6,2,29,9,21,45,27,92,91,44,40,28,82,29,92,12,74,25,12,5,55]), [92, 92]);
     });
 });
